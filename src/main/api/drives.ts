09122f4cd01794dda7dccaaed1e2e033145cd690
Marko Petzold <marko.petzold@record-evolution.de>
2024-10-23 10:25:58 +0200

    fix drives

--- src/main/api/drives.ts
+++ src/main/api/drives.ts
@@ -6,7 +6,6 @@ export async function listDrives() {
   const drives = await listdrives()
   return drives.filter(
     (d) =>
-      !d.mountpoints.find((m) => m.path.includes('boot')) &&
       d.busType !== 'UNKNOWN' &&
       !d.isSystem &&
       !d.isReadOnly
